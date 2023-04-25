2d12cdc7993b2cdf53f724f8ccf531a017eee7ea
saleh1992 <salehsites@gmail.com>
2023-04-25 15:41:39 +0200

    wikipedia-search-engine-debounce

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let resultsContainer = document.getElementsByClassName("container")[0]
-
+let debounce;
 const validateInput = (el) => {
-    if(el.value === ""){
+    clearTimeout(debounce)
+    if (el.value === "") {
         resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-    }else{
-        generateResults(el.value, el)
+    } else {
+        debounce = setTimeout(() => {
+            generateResults(el.value, el)
+        }, 1000)
     }
 }
 
@@ -13,25 +16,26 @@ const generateResults = (searchValue, inputField) => {
         "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
         + searchValue
     )
-    .then(response => response.json())
-    .then(data => {
-        let results = data.query.search
-        let numberOfResults = data.query.search.length
-        resultsContainer.innerHTML = ""
-        for(let i=0; i<numberOfResults; i++) {
-            let result = document.createElement("div")
-            result.classList.add("results")
-            result.innerHTML = `
+        .then(response => response.json())
+        .then(data => {
+            let results = data.query.search
+
+            let numberOfResults = data.query.search.length
+            resultsContainer.innerHTML = ""
+            for (let i = 0; i < numberOfResults; i++) {
+                let result = document.createElement("div")
+                result.classList.add("results")
+                result.innerHTML = `
             <div>
                 <h3>${results[i].title}</h3>
                 <p>${results[i].snippet}</p>
             </div>
             <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
             `
-            resultsContainer.appendChild(result)
-        }
-        if(inputField.value === ""){
-            resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-        }
-    })
+                resultsContainer.appendChild(result)
+            }
+            if (inputField.value === "") {
+                resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
+            }
+        })
 }
